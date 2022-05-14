1c4f8e1c3689171f630cb2ae052887e0ca6d2a2e
408210008 <91444520+408210008@users.noreply.github.com>
2022-05-14 15:21:07 +0800

    Create Search.c

---
+++ Search.c
@@ -0,0 +1,20 @@
+#include "myDS.h"
+
+
+void my_print_id(){
+	// Print the id of the corresponding Name.
+	// If Name doesn’t exit, print “INVALID OPERATION”
+	Store *p;
+  char Name[128];
+  scanf("%s",Name);
+	for (p = HEAD; p != NULL; p = p->next){
+		if (strcmp(p->name,Name)==0){
+			PRINT();
+			return;
+		}
+	}
+	if (p == NULL){
+		printf("INVALID OPERATION\n");
+		return;
+    }
+}
