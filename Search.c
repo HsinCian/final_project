3dc260525b2967918e3724324af4d059212b2be3
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-15 14:41:29 +0800

    Update Search.c

--- Search.c
+++ Search.c
@@ -1,19 +1,17 @@
 #include "myDS.h"
 
 
-void my_print_id(){
+void search(){
 	// If Name doesn’t exit, print “INVALID OPERATION”
 	Store *p;
   char Name[128];
   scanf("%s",Name);
-	for (p = HEAD; p != NULL; p = p->next){
-		if (strcmp(p->name,Name)==0){
-			//PRINT(); 這個好像要在外面
-			return;
-		}
-	}
-	if (p == NULL){
-		printf("INVALID OPERATION\n");
+  for (p = HEAD; p != NULL; p = p->next){
+	if (strcmp(p->name,Name)==0){
+		PRINT(); 這個好像要在外面
 		return;
-    }
+	}
+  }
+  printf("INVALID OPERATION\n");
+    
 }
