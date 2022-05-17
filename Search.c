7f73c0662562ee38c499be0019afacc96e22dfa9
408210008 <91444520+408210008@users.noreply.github.com>
2022-05-17 22:12:13 +0800

    Update Search.c

--- Search.c
+++ Search.c
@@ -8,7 +8,7 @@ void SEARCH(){
   scanf("%s",Name);
   for (p = HEAD; p != NULL; p = p->next){
 	if (strcmp(p->name,Name)==0){
-		PRINT(); 這個好像要在外面
+		printf("%s %s %lf %lf %f %d %d %lf %d\n",p->name,p->phonenumber,p->longitude,p->latitude,p->data,p->time[0],p->time[1],p->distance,p->price);
 		return;
 	}
   }
