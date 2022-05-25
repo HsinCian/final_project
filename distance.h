0b0b6a9935cd98e9864b8c94798c28691eb1a89f
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-25 19:25:53 +0800

    Create distance.h

---
+++ distance.h
@@ -0,0 +1,11 @@
+#ifndef DISTANCE_H
+#define DISTANCE_H
+
+double get_distance(double lat1, double lng1, double lat2, double lng2);
+
+void distance(Store *HEAD);
+
+void PRINTNEAR(Store *HEAD);
+
+
+#endif
