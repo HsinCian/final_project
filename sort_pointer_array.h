6b1b5f88776a1799771b42f574b3e2d824d49a05
TzuyenHuang <75783579+TzuyenHuang@users.noreply.github.com>
2022-05-25 13:42:39 +0800

    Create sort_pointer_array.h

---
+++ sort_pointer_array.h
@@ -0,0 +1,14 @@
+#ifndef SORT_POINTER_ARRAY_H
+#define SORT_POINTER_ARRAY_H
+
+int cmp_longitude(const void* a,const void* b);
+
+int cmp_latitude(const void* a,const void* b);
+
+int cmp_rating(const void* a,const void* b);
+
+//int cmp_distance(const void* a,const void* b);
+
+void SORT(Store **HEAD, char *sort_by, int count);
+
+#endif
