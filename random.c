8314963c247d2ec9eb5eacfc84f3969654965444
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-25 16:48:06 +0800

    Update random.c

--- random.c
+++ random.c
@@ -19,23 +19,21 @@ void RANDOM(Store *HEAD){
 }
 
 void RANDOMTYPE (Store *HEAD,int TYPE){
-	Store *p;
+    Store *p;
     int count=0,typenumber[100],i=0;
-	srand( time(NULL) );
-	int x = rand();
-	for(p = HEAD; p!= NULL; p = p->next){
-
-		if(p->type== TYPE){
-			typenumber[i]=count;
-			i++;
-		}
-		count++;
+    srand( time(NULL) );
+    int x = rand();
+    for(p = HEAD; p!= NULL; p = p->next){
+	if(p->type== TYPE){
+		typenumber[i]=count;
+		i++;
 	}
-
-	x=x%(i);
-	p = HEAD;
-	for(int i=0; i<typenumber[x]; i++)
-		p = p->next;
+	count++;
+    }
+    x=x%(i);
+    p = HEAD;
+    for(int i=0; i<typenumber[x]; i++)
+	p = p->next;
     char type[13];
     totype(type, p->type);
     printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
