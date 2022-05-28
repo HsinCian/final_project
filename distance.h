6ebfb64491a60724fc17f05fe37444905d059d2b
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-28 21:19:14 +0800

    Update distance.h

--- distance.h
+++ distance.h
@@ -1,6 +1,9 @@
 #ifndef DISTANCE_H
 #define DISTANCE_H
 
+#define PI                      3.1415926
+#define EARTH_RADIUS            6378.137        //地球近似半徑
+
 double get_distance(double lat1, double lng1, double lat2, double lng2);
 
 void distance(Store *HEAD);
