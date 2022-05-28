03136af6022d17e9e627dc0156d6fd76561db929
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-28 21:28:51 +0800

    Update distance.h

--- distance.h
+++ distance.h
@@ -4,9 +4,9 @@
 #define PI                      3.1415926
 #define EARTH_RADIUS            6378.137        //地球近似半徑
 
-double get_distance(double lat1, double lng1, double lat2, double lng2);
+double GET_DISTANCE(double lat1, double lng1, double lat2, double lng2);
 
-void distance(Store *HEAD);
+void DISTANCE(Store *HEAD);
 
 void PRINTNEAR(Store *HEAD);
 
