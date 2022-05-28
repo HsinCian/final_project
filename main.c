bdd946204c772843dba0186d3494a22e950512d4
Yann020315 <73629250+Yann020315@users.noreply.github.com>
2022-05-28 23:18:43 +0800

    Update main.c

--- main.c
+++ main.c
@@ -40,7 +40,7 @@ int main(){
       scanf("%s",t);
       if(strcmp("N",t)==0||strcmp("n",t)==0)PRINT(HEAD);
       else if(strcmp("Y",t)==0||strcmp("y",t)==0)WRITE_EXCEL(HEAD);
-      else printf("Please try again!\n");//Firstly writting data into output.csv. Secondly open the file by appropriate program such as Excel or Liberoffice.
+      else printf("Please try again!\n");//Firstly writting data into output.csv. Secondly opening the file by appropriate program such as Excel or Liberoffice.
     }
     else if(strcmp(cmd, "PRINT") == 0) PRINT(HEAD);
     else if(strcmp(cmd, "PRINTTYPE") == 0) PRINT_TYPE(HEAD);
