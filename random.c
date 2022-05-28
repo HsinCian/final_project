5d0e158d53b9213f9442db29b15c965c9a0a5490
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-28 23:48:50 +0800

    Update random.c

--- random.c
+++ random.c
@@ -15,6 +15,7 @@ void RANDOM(Store *HEAD){
 		p = p->next;
 	char type[13];
     	totype(type, p->type);
+	printf("NAME\t\t\t\tTYPE\t\t        PHONENUMBER\tLONGITUDE\tLATITUDE\tRATING\tOPEN\tCLOSE\tPRICE\n");
     	printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
     	printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
 }
@@ -40,6 +41,7 @@ void RANDOM_TYPE (Store *HEAD){
     for(int i=0; i<typenumber[x]; i++)
 	p = p->next;
     totype(type, p->type);
+    printf("NAME\t\t\t\tTYPE\t\t        PHONENUMBER\tLONGITUDE\tLATITUDE\tRATING\tOPEN\tCLOSE\tPRICE\n");
     printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
     printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
 }
@@ -100,6 +102,7 @@ void RANDOM_NEAR(Store *HEAD){
 		p = p->next;
 	char type[13];
     totype(type, p->type);
+    printf("NAME\t\t\t\tTYPE\t\t        PHONENUMBER\tLONGITUDE\tLATITUDE\tRATING\tOPEN\tCLOSE\tPRICE\n");
     printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
     printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
 
