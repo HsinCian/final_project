7a34cf3c44cf2516b4680cdd80766be31de5c776
408210008 <91444520+408210008@users.noreply.github.com>
2022-05-29 21:35:28 +0800

    Update distance.c

--- distance.c
+++ distance.c
@@ -1,5 +1,6 @@
 #include "struct.h"
 #include "myIO.h"
+#include "insert.h"
 #include "distance.h"
 #include <math.h>
 
@@ -145,6 +146,7 @@ void PRINTNEAR(Store *HEAD){
             totype(type, p->type);
             printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
             printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
+	    IS_OPEN(p);
           }
     }
 }
