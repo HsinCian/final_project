481d1f271c646c4cc5f3ceefee88700a4def47bc
408210008 <91444520+408210008@users.noreply.github.com>
2022-05-29 23:22:15 +0800

    Update my_habit.c

--- my_habit.c
+++ my_habit.c
@@ -42,6 +42,7 @@ void MY_HABIT(Store *HEAD, int *rec){
     	totype(type, p -> type);
     	printf("NAME\t\t\t\tTYPE\t\t        PHONENUMBER\tLONGITUDE\tLATITUDE\tRATING\tOPEN\tCLOSE\tPRICE\n");
     	printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
-    	printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
+    	printf("\t%04d\t%04d\t%d\t",p->time[0],p->time[1],p->price);
 	IS_OPEN(p);
+	printf("\n");
 }
