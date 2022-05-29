5117ba91aae7c81f41cc125941ceb1b355e54950
408210008 <91444520+408210008@users.noreply.github.com>
2022-05-29 21:37:37 +0800

    Update random.c

--- random.c
+++ random.c
@@ -1,5 +1,6 @@
 #include "struct.h"
 #include "myIO.h"
+#include "insert.h"
 #include "distance.h"
 #include "random.h"
 
@@ -18,6 +19,7 @@ void RANDOM(Store *HEAD){
 	printf("NAME\t\t\t\tTYPE\t\t        PHONENUMBER\tLONGITUDE\tLATITUDE\tRATING\tOPEN\tCLOSE\tPRICE\n");
     	printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
     	printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
+	IS_OPEN(p);
 }
 
 void RANDOM_TYPE (Store *HEAD){
@@ -44,6 +46,7 @@ void RANDOM_TYPE (Store *HEAD){
     printf("NAME\t\t\t\tTYPE\t\t        PHONENUMBER\tLONGITUDE\tLATITUDE\tRATING\tOPEN\tCLOSE\tPRICE\n");
     printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
     printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
+    IS_OPEN(p);
 }
 
 void RANDOM_NEAR(Store *HEAD){
@@ -118,6 +121,6 @@ void RANDOM_NEAR(Store *HEAD){
     printf("NAME\t\t\t\tTYPE\t\t        PHONENUMBER\tLONGITUDE\tLATITUDE\tRATING\tOPEN\tCLOSE\tPRICE\n");
     printf("%-30s\t%-12s\t\t%s\t%f\t%f\t%.1f",p->name,type,p->phonenumber,p->longitude,p->latitude,p->rating);
     printf("\t%04d\t%04d\t%d\n",p->time[0],p->time[1],p->price);
-
+    IS_OPEN(p);
 
 }
