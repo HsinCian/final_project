51cd5aa18279e1232e9fce3b12ca23602849149b
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-30 22:58:54 +0800

    Update main.c

--- main.c
+++ main.c
@@ -57,7 +57,7 @@ int main(){
     else if(strcmp(cmd, "PRINT") == 0)      PRINT(HEAD);
     else if(strcmp(cmd, "PRINTTYPE") == 0)  PRINT_TYPE(HEAD);
     else if(strcmp(cmd, "RANDOM") == 0)     RANDOM(HEAD);
-    else if(strcmp(cmd, "PRINTNEAR") == 0)     PRINTNEAR(HEAD,lat1,lng1);
+    else if(strcmp(cmd, "PRINTNEAR") == 0)     PRINT_NEAR(HEAD,lat1,lng1);
     else if(strcmp(cmd, "RANDOMTYPE") == 0) RANDOM_TYPE(HEAD);
     else if(strcmp(cmd, "DISTANCE") == 0)   DISTANCE(HEAD,lat1,lng1);
     else if(strcmp(cmd, "RANDOMNEAR") == 0) RANDOM_NEAR(HEAD,lat1,lng1);
