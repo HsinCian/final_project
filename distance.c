6098ea05af3c3fd820e0bfbe5841049c0eb376ac
OK that fine <97590256+saulkuo123@users.noreply.github.com>
2022-05-30 22:58:01 +0800

    Update distance.c

--- distance.c
+++ distance.c
@@ -134,7 +134,7 @@ void DISTANCE(Store *HEAD,double lat1, double lng1){
     }	
 }
 
-void PRINTNEAR(Store *HEAD,double lat1, double lng1){
+void PRINT_NEAR(Store *HEAD,double lat1, double lng1){
     double lat2;
     double lng2;
     Store *p;
