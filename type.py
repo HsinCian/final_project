c518d6d7f1c9008d1942cdf3ca8c61f419375656
HsinCian <73248238+HsinCian@users.noreply.github.com>
2022-06-01 00:03:11 +0800

    Update type.py

--- type.py
+++ type.py
@@ -4,7 +4,7 @@ import os, sys
 
 window = tk.Tk()
 window.title('type')
-window.geometry('800x550')
+window.geometry('300x500')
 window.configure(background='white')
 
 header_label = tk.Label(window, text='Choose your type')
