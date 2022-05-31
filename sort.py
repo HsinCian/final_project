de3443e170d8daa14570becf05a45c89fa02ffb2
HsinCian <73248238+HsinCian@users.noreply.github.com>
2022-05-31 21:30:16 +0800

    Add files via upload

---
+++ sort.py
@@ -0,0 +1,36 @@
+import tkinter as tk
+import math
+import os, sys
+
+window = tk.Tk()
+window.title('sort')
+window.geometry('800x550')
+window.configure(background='white')
+
+header_label = tk.Label(window, text='Sort by what?')
+header_label.pack()
+
+fo = open("input.txt", "w")
+
+def alll():
+    fo.write('longitude ')
+    fo.close()
+    sys.exit(0)
+    
+def typee():
+    fo.write('latitude ')
+    fo.close()
+    sys.exit(0)
+    
+def near():
+    fo.write('rating ')
+    fo.close()
+    sys.exit(0)
+
+button1 = tk.Button(window, text='longitude', command=alll).pack(padx=5, pady=10)
+button2 = tk.Button(window, text='latitude', command=typee).pack(padx=5, pady=10)
+button3 = tk.Button(window, text='rating', command=near).pack(padx=5, pady=10)
+
+window.mainloop()
+
+
