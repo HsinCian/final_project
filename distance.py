d3f49f96058431117d385c9f4aa01a3c5c9df49d
408210008 <91444520+408210008@users.noreply.github.com>
2022-05-31 10:37:41 +0800

    Create distance.py

---
+++ distance.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+import math
+import os, sys
+
+window = tk.Tk()
+window.title('distance')
+window.geometry('800x550')
+window.configure(background='white')
+
+
+fo = open("input.txt", "w")
+
+def event():
+    fo.write( name_entry.get())
+    fo.close()
+    sys.exit(0)
+
+name_frame = tk.Frame(window)
+name_frame.pack(side=tk.TOP, padx=5, pady=10)
+name_label = tk.Label(name_frame, text='name')
+name_label.pack(side=tk.LEFT)
+name_entry = tk.Entry(name_frame)
+name_entry.pack(side=tk.LEFT)
+
+
+button = tk.Button(window, text='distance', command=event).pack(padx=5, pady=10)
+
+window.mainloop()
+
+fo.close()
