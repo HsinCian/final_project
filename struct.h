fec3ee61545e1e318131d101d79b634eefa7e249
TzuyenHuang <75783579+TzuyenHuang@users.noreply.github.com>
2022-06-02 14:36:12 +0800

    Update struct.h

--- struct.h
+++ struct.h
@@ -7,16 +7,15 @@
 #include <time.h>
 
 typedef struct store{
-  char name[128]; //店家名稱
+  char name[128];         //店家名稱
   enum TYPE{CHICKEN_RICE, JAPANESE, KOREAN, AMERICAN, CHINESE, BREAKFAST, DRINKS, DESSERT}type;
-  char phonenumber[11]; //電話
-  double longitude; //經度
-  double latitude; //緯度
-  float rating; //評價
-  int time[2]; //營業時間
-  double distance; //單位:公里 (與CCU csie的距離)
-  int price; //大概的價位
-  struct store *next; //Linkedlist
+  char phonenumber[11];   //電話
+  double longitude;       //經度
+  double latitude;        //緯度
+  float rating;           //評價
+  int time[2];            //營業時間
+  int price;              //大概的價位(0:0~100 / 1:100~300 / 2:300~500 / 3:500以上)
+  struct store *next;     //Linkedlist
 }Store;
 
 #endif
